33e9c4ffcbb89e59d38e419cc62273d3fbdcc9da
Antonio Lazaro Carvalho Borges <antonio.lazaro@gmail.com>
2015-12-09 02:14:12 -0300

    parametrizacao do banco de dados.

--- inventarioChescoved/src/br/com/vortice/chescoved/inventario/sqlloader/CreateDatabaseStructure.java
+++ inventarioChescoved/src/br/com/vortice/chescoved/inventario/sqlloader/CreateDatabaseStructure.java
@@ -1,19 +1,30 @@
 package br.com.vortice.chescoved.inventario.sqlloader;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.sql.Connection;
+import java.util.Properties;
 
+import org.h2.store.fs.FileUtils;
 import org.h2.tools.RunScript;
 
 import br.com.vortice.chescoved.inventario.dao.DAOAb;
 
 public class CreateDatabaseStructure {
 	
-	public static final String DATABASE_PATH = "~/inventario_chescoved";
+	public static String DATABASE_PATH = null;
+	static{
+		try {
+			DATABASE_PATH = getPath()+File.separator+"inventario_chescoved";
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public static void initDatabase() throws Exception{
-		Connection connection = new DAOAb().getDBConnection();
-//		if(!FileUtils.exists(DATABASE_PATH+".h2.db")){
+		if(!FileUtils.exists(DATABASE_PATH+".h2.db")){
+			Connection connection = new DAOAb().getDBConnection();
 			executeScript(connection,"database/create-database.sql");
 			executeScript(connection,"database/create-database-arc.sql");
 			executeScript(connection,"database/create-database-material_interno.sql");
@@ -25,7 +36,18 @@ public class CreateDatabaseStructure {
 			executeScript(connection,"database/carga-tabelas_produtos_estoque_material_interno.sql");
 			
 			connection.close();
-//		}
+		}
+	}
+	
+	private static String getPath() throws Exception{
+		Properties mainProperties = new Properties();
+
+		String path = "./path.properties";
+	    FileInputStream file = new FileInputStream(path);
+	    mainProperties.load(file);
+	    file.close();
+	    return mainProperties.getProperty("path");
+
 	}
 	
 	private static void executeScript(Connection connection,String fileReaderPath){
